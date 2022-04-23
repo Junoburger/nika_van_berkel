9ea69d9b26b491934104e42f9c1d14c5061a92dc
Junoburger <juno.burger@pm.me>
2022-04-23 02:46:28 +0200

    Fix hover

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -8,65 +8,54 @@ const Icon = ({ blok }) => {
 	const [displayTitle, setDisplayTitle] = useState(false);
 
 	return (
-		<div>
+		<ProjectContainer>
 			<Link href={`/projects/${blok.url_parameter}`}>
 				<div>
 					{blok.icon.filename ? (
-						<Link href={`/projects/${blok.url_parameter}`}>
-							<div>
-								<ImageWrapper>
-									<Image
-										src={blok.icon.filename}
-										alt={blok.icon.alt}
-										width={250}
-										height={250}
-										onMouseOut={() => setDisplayTitle(false)}
-										onMouseEnter={() => setDisplayTitle(true)}
-									/>
-									{displayTitle && (
-										<Title displayTitle={displayTitle}>
-											{blok.project_name}
-										</Title>
-									)}
-								</ImageWrapper>
-							</div>
-						</Link>
+						<div>
+							{/* <ImageWrapper> */}
+							<img
+								onMouseOut={() => setDisplayTitle(false)}
+								onMouseEnter={() => setDisplayTitle(true)}
+								src={blok.icon.filename}
+								alt={blok.icon.alt}
+								width={250}
+								height={250}
+							/>
+
+							<Title displayTitle={displayTitle}>{blok.project_name}</Title>
+							{/* </ImageWrapper> */}
+						</div>
 					) : (
 						<div>placeholder</div>
 					)}
 				</div>
 			</Link>
-		</div>
+		</ProjectContainer>
 	);
 };
 
 export default Icon;
 
 const ImageWrapper = styled.div`
-	display: flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	text-align: center;
-	height: 80%;
-	min-width: 25%;
-	justify-content: center;
-	text-align: center;
+	/* display: flex; */
+	/* flex-direction: column; */
+	/* flex-wrap: wrap; */
+	/* text-align: center; */
+	/* height: 115%; */
+	/* width: 110%; */
+	/* justify-content: center; */
+	/* text-align: center; */
 	img {
-		/* border: 5px solid black;
-		max-width: 15rem;
+		/* border: 5px solid black; */
+		/* max-width: 15rem;
 		max-height: 15rem;
 		margin: 3rem; */
 	}
 `;
 
-const ProjectWrapper = styled.div`
-	position: relative;
+export const Title = styled.div<{ displayTitle: boolean }>`
+	color: ${(props) => (props.displayTitle ? "black" : "white")};
 `;
 
-export const Title = styled.div<{ displayTitle: boolean }>`
-	/* display: none; */
-	/* color: white; */
-	${ProjectWrapper}:hover & {
-		/* color: ${(props) => (props.displayTitle ? "black" : "white")}; */
-	}
-`;
+const ProjectContainer = styled.div``;
